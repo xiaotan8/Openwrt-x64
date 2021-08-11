bbc7eeee25e4165a6d2fa287050a71a2518a13fb
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2021-08-11 21:34:50 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Apply custom.sh"
-git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06                                     package/luci-theme-argon-jerrykuku
+git clone https://github.com/jerrykuku/luci-theme-argon.git -b master                                     package/luci-theme-argon-jerrykuku
 git clone https://github.com/vernesong/OpenClash.git -b master                                           package/luci-app-openclash
 git clone https://github.com/destan19/OpenAppFilter.git                                                  package/luci-app-oaf      
 
