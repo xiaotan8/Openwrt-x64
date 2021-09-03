781c89193d5198251753d7731d262559673f96ad
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2021-09-03 22:07:25 +0800

    Delete user/lean-x64 directory

--- user/lean-x64/custom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Test custom.sh"
-git clone https://github.com/pymumu/luci-app-smartdns.git -b lede                        package/luci-app-smartdns
-svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns                                      package/smartdns
-
-rm -rf feeds/packages/libs/libcap
-svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap feeds/packages/libs/libcap
