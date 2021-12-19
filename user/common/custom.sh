26b2d9f1913d66a81a1c518803118ce29421b92c
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2021-12-19 12:54:29 +0800

    fix

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Apply custom.sh"
-rm -rf luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git                                       luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git                                       feeds/luci/themes/luci-theme-argon
