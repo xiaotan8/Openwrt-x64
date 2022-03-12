1c733586a7be6ded276413771595590f599169e8
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2022-03-13 00:49:21 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -4,6 +4,6 @@ echo "Test custom.sh"
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
 git clone https://github.com/destan19/OpenAppFilter.git           package/diy/OpenAppFilter
-git clone https://github.com/xiaorouji/openwrt-passwall.git       package/diy/passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git      package/diy/passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages      package/diy/packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci     package/diy/passwall
 git clone https://github.com/vernesong/OpenClash.git              package/diy/openclash
