b967165c5daf93d40c3b56acbb08c091b02c9957
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2022-04-05 03:59:14 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -3,6 +3,8 @@
 echo "Test custom.sh"
 rm -rf feeds/packages/kernel/antfs
 rm -rf package/feeds/packages/antfs-mount
+rm -rf package/feeds/other/automount
+svn co https://github.com/coolsnowwolf/lede/tree/master/package/lean/automount   package/feeds/other/automount
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ntfs3-mount feeds/packages/kernel/ntfs3-mount
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ntfs3-oot   feeds/packages/kernel/ntfs3-oot
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git   package/luci-app-jd-dailybonus
