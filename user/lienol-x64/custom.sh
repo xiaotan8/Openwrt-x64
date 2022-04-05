a9d555703f851279e946430326480feda3d7062e
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2022-04-05 16:32:12 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 echo "Test custom.sh"
-rm -rf feeds/packages/kernel/antfs
-rm -rf package/feeds/packages/antfs-mount
-rm -rf package/feeds/other/automount
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/automount   package/feeds/other/automount
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ntfs3-mount feeds/packages/kernel/ntfs3-mount
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ntfs3-oot   feeds/packages/kernel/ntfs3-oot
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git   package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
 git clone https://github.com/destan19/OpenAppFilter.git             package/diy/OpenAppFilter
