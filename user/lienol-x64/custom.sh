7e1955265fb8952619f10f13562c34be1e460cf8
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2022-08-27 19:26:15 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 echo "Test custom.sh"
-# rm -rf feeds/luci/themes/luci-theme-argon
-git clone https://github.com/fw876/helloworld.git                               package/helloworld
-# git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06          feeds/luci/themes/luci-theme-argontyfreedom
-rm -rf feeds/packages/libs/libcap
-svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap             feeds/packages/libs/libcap
-git clone https://github.com/pymumu/luci-app-smartdns.git -b lede               package/applications/luci-app-smartdns
-git clone https://github.com/tty228/luci-app-serverchan.git                     package/applications/luci-app-serverchan
-svn co https://github.com/Lienol/openwrt-package/branches/other/luci-app-adguardhome            package/applications/luci-app-adguardhome
-git clone https://github.com/destan19/OpenAppFilter.git                         package/OpenAppFilter
-git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages         package/passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci             package/passwall
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash          package/luci-app-openclash
-rm -rf feeds/packages/net/mosdns
-git clone https://github.com/QiuSimons/openwrt-mos.git                          package/openwrt-mos
-git clone https://github.com/KFERMercer/luci-app-tcpdump.git                    package/luci-app-tcpdump
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git       feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
+git clone https://github.com/destan19/OpenAppFilter.git           package/diy/OpenAppFilter
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages      package/diy/passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci     package/diy/passwall
+# git clone  https://github.com/fw876/helloworld.git                package/helloworld
