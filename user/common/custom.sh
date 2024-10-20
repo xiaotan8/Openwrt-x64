ac58599fcc641ba6b6ae3969bdcef20ee7eeaf30
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2024-10-20 16:50:37 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 echo "Apply custom.sh"
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git                                       feeds/luci/themes/luci-theme-argon
