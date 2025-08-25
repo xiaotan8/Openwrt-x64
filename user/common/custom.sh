0454a649936dd4292f1fef2bec44640318664580
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-08-25 20:20:31 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Apply custom.sh"
+mkdir -p openwrt/feeds/packages/lang/rust/patches
+cat > openwrt/feeds/packages/lang/rust/patches/010-disable-ci-llvm.patch <<'EOF'
+--- a/src/bootstrap/config.toml.example
++++ b/src/bootstrap/config.toml.example
+@@ -146,7 +146,7 @@
+ # Whether to download prebuilt LLVM from CI. This is only supported on
+ # 64-bit x86 platforms, and requires curl and tar to be installed.
+ #
+-# download-ci-llvm = true
++download-ci-llvm = "if-unchanged"
+EOF
