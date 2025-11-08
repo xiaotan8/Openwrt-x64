6f15835107c9a71dcb9d133816f89a36483e5e18
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2025-11-09 01:01:41 +0800

    Update minimum CMake version to 3.10

--- user/OpenWrtSNAPSHOT/custom.sh
+++ user/OpenWrtSNAPSHOT/custom.sh
@@ -89,7 +89,7 @@ fix_libwebsockets_cmake() {
  # Minimum cmake required
  # ----------------------
 -cmake_minimum_required(VERSION 2.8.12)
-+cmake_minimum_required(VERSION 3.5)
++cmake_minimum_required(VERSION 3.10)
 
 EOF
 
