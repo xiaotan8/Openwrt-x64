f5d99dad33a25ae2ba8ca65f73af8ec2759caf91
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2026-03-04 19:08:37 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -45,7 +45,7 @@ git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo.git  package/
 git clone --depth=1 https://github.com/xiaotan8/luci-app-vlmcsd.git package/applications/luci-app-vlmcsd
 git clone --depth=1 https://github.com/sirpdboy/luci-app-timecontrol.git package/applications/luci-app-timecontrol
 git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp.git  package/applications/luci-app-partexp
-git clone --depth=1 https://github.com/sirpdboy/luci-app-netspeedtest.git package/applications/luci-app-netspeedtest
+git clone --depth=1 https://github.com/muink/luci-app-netspeedtest.git package/applications/luci-app-netspeedtest
 git clone --depth=1 https://github.com/xiaotan8/vlmcsd.git package/vlmcsd
 git clone --depth=1 https://github.com/xiaotan8/wrtbwmon.git package/wrtbwmon
 
