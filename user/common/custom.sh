087d6335c922d4084db35f5bfeccc7031c8ea60c
xiaotan8 <62252056+xiaotan8@users.noreply.github.com>
2026-05-20 19:13:47 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -10,7 +10,7 @@ echo "=============================="
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf package/luci-theme-argon
 rm -rf package/applications/luci-app-argon-config
-rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # ==============================
 # 2. 拉取主题 & 配置插件
 # ==============================
